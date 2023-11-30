7e6c9214049ccc31f3ab4260b20d17f102bd4cc6
YapWH1208 <107160166+YapWH1208@users.noreply.github.com>
2023-11-30 18:52:42 +0800

    Update utils.py
    - logger
    - lastest ckpt

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,8 @@ import json
 import os
 import torch
 from model import ViT
+import logging
+import re
 
 
 def save_checkpoint(experiment_name:str, model, epoch:int, base_dir:str="experiments"):
@@ -53,3 +55,38 @@ def load_experiment(experiment_name:str, checkpoint_name:str="model_final.pt", b
     cpfile = os.path.join(outdir, checkpoint_name)
     model.load_state_dict(torch.load(cpfile))
     return config, model, train_losses, test_losses, accuracies
+
+
+def set_logger(log_path):
+    """Set the logger to log info in terminal and file `log_path`.
+    In general, it is useful to have a logger so that every output to the terminal is saved
+    in a permanent file. Here we save it to `model_dir/train.log`.
+    Example:
+    ```
+    logging.info("Starting training...")
+    ```
+    Args:
+        log_path: (string) where to log
+    """
+    if os.path.exists(log_path) is True:
+        os.remove(log_path)
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    if not logger.handlers:
+        # Logging to a file
+        file_handler = logging.FileHandler(log_path)
+        file_handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s: %(message)s'))
+        logger.addHandler(file_handler)
+
+        # Logging to console
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(logging.Formatter('%(message)s'))
+        logger.addHandler(stream_handler)
+
+def lastest_checkpoint(model_dir="./experiments"):
+    files = [f for f in os.listdir(model_dir) if 'model' in f]
+    files.sort(key=lambda x: int(re.findall(r'\d+', x)[0]))
+    if len(files) == 0:
+        return None
+    return os.path.join(model_dir, files[-1])
