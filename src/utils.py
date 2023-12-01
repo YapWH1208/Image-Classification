23065b29a5e29891bbb2704e06030e580f029aab
YapWH1208 <107160166+YapWH1208@users.noreply.github.com>
2023-12-02 00:03:03 +0800

    Update utils.py
    - delete load_exp
    - folder name

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,11 @@
-import json
 import os
-import torch
-from model import ViT
-import logging
 import re
+import torch
+import logging
+from datetime import datetime
 
 
-def save_checkpoint(experiment_name:str, model, epoch:int, base_dir:str="experiments"):
+def save_checkpoint(model, epoch:int, base_dir:str="experiments"):
     """
     Saves the model checkpoint
 
@@ -19,42 +18,10 @@ def save_checkpoint(experiment_name:str, model, epoch:int, base_dir:str="experim
     Returns:
     None
     """
-    outdir = os.path.join(base_dir, experiment_name)
+    outdir = os.path.join(base_dir, datetime.today().strftime('%Y-%m-%d'))
     os.makedirs(outdir, exist_ok=True)
-    cpfile = os.path.join(outdir, f'model_{epoch}.pt')
+    cpfile = os.path.join(outdir, f'model_{epoch:.3f}.pt')
     torch.save(model.state_dict(), cpfile)
-
-
-def load_experiment(experiment_name:str, checkpoint_name:str="model_final.pt", base_dir:str="experiments"):
-    """
-    Loads the experiment from the saved checkpoint
-
-    Args:
-    experiment_name (str): Name of the experiment
-    checkpoint_name (str, optional): Name of the checkpoint. Defaults to "model_final.pt".
-    base_dir (str, optional): Base directory where the experiment is saved. Defaults to "experiments".
-
-    Returns:
-    config (dict): Dictionary containing the config of the experiment
-    model (ViT): The model used for the experiment
-    train_losses (list): List containing the training losses
-    test_losses (list): List containing the test losses
-    accuracies (list): List containing the accuracies
-    """
-    outdir = os.path.join(base_dir, experiment_name)
-    configfile = os.path.join(outdir, 'config.json')
-    with open(configfile, 'r') as f:
-        config = json.load(f)
-    jsonfile = os.path.join(outdir, 'metrics.json')
-    with open(jsonfile, 'r') as f:
-        data = json.load(f)
-    train_losses = data['train_losses']
-    test_losses = data['test_losses']
-    accuracies = data['accuracies']
-    model = ViT(config)
-    cpfile = os.path.join(outdir, checkpoint_name)
-    model.load_state_dict(torch.load(cpfile))
-    return config, model, train_losses, test_losses, accuracies
 
 
 def set_logger(log_path):
@@ -83,6 +50,7 @@ def set_logger(log_path):
         stream_handler = logging.StreamHandler()
         stream_handler.setFormatter(logging.Formatter('%(message)s'))
         logger.addHandler(stream_handler)
+
 
 def lastest_checkpoint(model_dir="./experiments"):
     files = [f for f in os.listdir(model_dir) if 'model' in f]
