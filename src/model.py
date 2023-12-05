dc5057d7170831fefae48eed69f96b5131df136d
YapWH1208 <107160166+YapWH1208@users.noreply.github.com>
2023-12-05 09:22:46 +0800

    Update model.py
    fixes

--- src/model.py
+++ src/model.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-class Encoder():
+class Encoder(nn.Module):
     """
     Encoder class for the ViT model
 
@@ -24,7 +24,7 @@ class Encoder():
         super(Encoder, self).__init__()
         self.blocks = nn.ModuleList([])
         for _ in range(num_hidden_layers):
-            block = EncoderBlock(hidden_size, intermediate_size, num_attention_heads, attention_probs_dropout_prob, hidden_dropout_prob, qkv_bias)
+            block = EncoderBlock(hidden_size, intermediate_size, num_attention_heads, attention_probs_dropout_prob, hidden_dropout_prob, qkv_bias).to(device)
             self.blocks.append(block)
 
 
@@ -51,9 +51,9 @@ class EncoderBlock(nn.Module):
     """
     def __init__(self, hidden_size:int, intermediate_size:int, num_attention_heads:int=4, attention_probs_dropout_prob:float=0.0, hidden_dropout_prob:float=0.0, qkv_bias=True):
         super(EncoderBlock, self).__init__()
-        self.norm1 = nn.LayerNorm(hidden_size).to(device)
+        self.norm1 = nn.LayerNorm(hidden_size)
         self.MHA = MultiHeadAttention(hidden_size, num_attention_heads, attention_probs_dropout_prob, hidden_dropout_prob, qkv_bias).to(device)
-        self.norm2 = nn.LayerNorm(hidden_size).to(device)
+        self.norm2 = nn.LayerNorm(hidden_size)
         self.mlp = MLP(hidden_size, intermediate_size, hidden_dropout_prob).to(device)
     
     def forward(self, X):
@@ -84,10 +84,10 @@ class MLP(nn.Module):
     """
     def __init__(self, hidden_size:int, intermediate_size:int, hidden_dropout_prob:int=0.0):
         super(MLP, self).__init__()
-        self.fc1 = nn.Linear(hidden_size, intermediate_size).to(device)
-        self.GELU = nn.GELU().to(device)
-        self.fc2 = nn.Linear(intermediate_size, hidden_size).to(device)
-        self.dropout = nn.Dropout(hidden_dropout_prob).to(device)
+        self.fc1 = nn.Linear(hidden_size, intermediate_size)
+        self.GELU = nn.GELU()
+        self.fc2 = nn.Linear(intermediate_size, hidden_size)
+        self.dropout = nn.Dropout(hidden_dropout_prob)
 
     def forward(self, X):
         X = self.fc1(X)
@@ -116,9 +116,9 @@ class Scaled_Dot_Product_Attention(nn.Module):
         self.hidden_size = hidden_size
         self.attention_head_size = attention_head_size
 
-        self.query = nn.Linear(hidden_size, attention_head_size, bias=bias).to(device)
-        self.key = nn.Linear(hidden_size, attention_head_size, bias=bias).to(device)
-        self.value = nn.Linear(hidden_size, attention_head_size, bias=bias).to(device)
+        self.query = nn.Linear(hidden_size, attention_head_size, bias=bias)
+        self.key = nn.Linear(hidden_size, attention_head_size, bias=bias)
+        self.value = nn.Linear(hidden_size, attention_head_size, bias=bias)
 
         self.dropout = nn.Dropout(dropout_prob)
     
@@ -160,12 +160,12 @@ class MultiHeadAttention(nn.Module):
         self.qkv_bias = qkv_bias
         self.heads = nn.ModuleList([])
         for _ in range(self.num_attention_heads):
-            head = Scaled_Dot_Product_Attention(self.hidden_size, self.attention_head_size, attention_probs_dropout_prob, bias=self.qkv_bias)
+            head = Scaled_Dot_Product_Attention(self.hidden_size, self.attention_head_size, attention_probs_dropout_prob, bias=self.qkv_bias).to(device)
             self.heads.append(head)
         
-        self.output_projection = nn.Linear(self.all_head_size, self.hidden_size).to(device)
-        self.output_dropout = nn.Dropout(hidden_dropout_prob).to(device)
-    
+        self.output_projection = nn.Linear(self.all_head_size, self.hidden_size)
+        self.output_dropout = nn.Dropout(hidden_dropout_prob)
+
     def forward(self, X):
         attention_outputs = [head(X) for head in self.heads]
         attention_output = torch.cat([attention_output for attention_output, _ in attention_outputs], dim=-1)
@@ -195,7 +195,7 @@ class PatchEmbeddings(nn.Module):
         self.hidden_size = hidden_size
         self.num_channels = num_channels
         self.num_patches = (self.image_size // self.patch_size) * (self.image_size // self.patch_size)
-        self.projection = nn.Conv2d(self.num_channels, self.hidden_size, kernel_size=self.patch_size, stride=self.patch_size).to(device)
+        self.projection = nn.Conv2d(self.num_channels, self.hidden_size, kernel_size=self.patch_size, stride=self.patch_size)
     
     def forward(self, X):
         X = self.projection(X)
@@ -221,9 +221,9 @@ class Embeddings(nn.Module):
     def __init__(self, hidden_size:int, image_size:int, patch_size:int=16, num_channels:int=3, hidden_dropout_prob:float=0.0):
         super(Embeddings, self).__init__()
         self.patch_embeddings = PatchEmbeddings(image_size, hidden_size, patch_size, num_channels).to(device)
-        self.cls_token = nn.Parameter(torch.randn(1, 1, hidden_size)).to(device)
-        self.position_embeddings = nn.Parameter(torch.randn(1, self.patch_embeddings.num_patches+1, hidden_size)).to(device)
-        self.dropout = nn.Dropout(hidden_dropout_prob).to(device)
+        self.cls_token = nn.Parameter(torch.randn(1, 1, hidden_size))
+        self.position_embeddings = nn.Parameter(torch.randn(1, self.patch_embeddings.num_patches+1, hidden_size))
+        self.dropout = nn.Dropout(hidden_dropout_prob)
 
     def forward(self, X):
         X = self.patch_embeddings(X)
@@ -255,20 +255,18 @@ class ViT(nn.Module):
     Returns:
     logits (Tensor): Tensor containing the logits
     """
-    def __init__(self, image_size:int, hidden_size:int, num_hidden_layers:int, intermediate_size, num_classes:int, num_attention_heads:int=4, hidden_dropout_prob:float=0.0, 
-                 attention_probs_dropout_prob:float=0.0, num_channels:int=3, patch_size:int=16, qkv_bias:bool=True):
+    def __init__(self, image_size:int, hidden_size:int, num_hidden_layers:int, intermediate_size, num_classes:int, num_attention_heads:int=4, 
+                 hidden_dropout_prob:float=0.0, attention_probs_dropout_prob:float=0.0, num_channels:int=3, patch_size:int=16, qkv_bias:bool=True):
         super(ViT, self).__init__()
-        self.image_size = image_size
-        self.hidden_size = hidden_size
-        self.num_channels = num_channels
         self.embeddings = Embeddings(hidden_size, image_size, patch_size, num_channels, hidden_dropout_prob).to(device)
-        self.encoder = Encoder(num_hidden_layers, hidden_size, intermediate_size, num_attention_heads, attention_probs_dropout_prob, hidden_dropout_prob, qkv_bias)
+        self.encoder = Encoder(num_hidden_layers, hidden_size, intermediate_size, num_attention_heads, 
+                               attention_probs_dropout_prob, hidden_dropout_prob, qkv_bias).to(device)
         self.classifier = nn.Linear(hidden_size, num_classes).to(device)
         self.apply(self._init_weights)
 
     def forward(self, X):
         embedding_output = self.embeddings(X)
-        encoder_output = self.encoder.forward(embedding_output)
+        encoder_output = self.encoder(embedding_output)
         logits = self.classifier(encoder_output[:, 0, :])
         return logits
     
